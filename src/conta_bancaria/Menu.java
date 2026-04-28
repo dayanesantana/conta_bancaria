fd58d53b34e283b37f10c4668cfc08bd31774e75
Dayane Santana <dayanesantana59@gmail.com>
2026-04-28 12:54:54 -0300

    Commit inicial - Projeto Conta Bancária

---
+++ src/conta_bancaria/Menu.java
@@ -0,0 +1,93 @@
+package conta_bancaria;
+
+import java.util.Scanner;
+
+public class Menu {
+
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+		int opcao;
+		
+		while (true) {
+			System.out.println("***********************************************");
+			System.out.println("                                               ");
+			System.out.println("             BANCO DO BRAZIL COM Z             ");
+			System.out.println("                                               ");
+			System.out.println("***********************************************");
+			System.out.println("                                               ");
+			System.out.println("        1 - Criar conta                        ");
+			System.out.println("        2 - Listar todas as contas             ");
+			System.out.println("        3 - Buscar conta por número            ");
+			System.out.println("        4 - Atualizar dados da conta           ");
+			System.out.println("        5 - Apagar conta                       ");
+			System.out.println("        6 - Sacar                              ");
+			System.out.println("        7 - Depositar                          ");
+			System.out.println("        8 - Transferir valores entre contas    ");
+			System.out.println("        0 - Sair                               ");
+			System.out.println("                                               ");
+			System.out.println("***********************************************");
+			System.out.println("ENTRE COM A OPÇÃO DESEJADA:                    ");
+			System.out.println("                                               ");
+			
+			opcao = leia.nextInt();
+			
+			if (opcao == 0) {
+				System.out.println("\nBanco do Brazil com Z - O seu futuro começa aqui!");
+				sobre();
+				leia.close();
+				System.exit(0);
+			}
+			
+			switch(opcao) {
+			case 1:
+				System.out.println("Criar conta \n\n");
+				break;
+				
+			case 2:
+				System.out.println("Listar todas as contas \n\n");
+				break;
+				
+			case 3:
+				System.out.println("Consultar dados da conta por número \n\n");
+				break;
+				
+			case 4:
+				System.out.println("Atualizar dados da conta \n\n");
+				break;
+				
+			case 5:
+				System.out.println("Apagar conta \n\n");
+				break;
+				
+			case 6:
+				System.out.println("Saque \n\n");
+				break;
+				
+			case 7:
+				System.out.println("Depósito\n\n");
+				break;
+				
+			case 8:
+				System.out.println("Transferencia entre contas\n\n");
+				break;
+				
+			default:
+				System.out.println("\nOpção Inválida!\n");
+				break;
+			}
+		}
+		
+	}
+		public static void sobre() {
+			System.out.println("\n***********************************************");
+			System.out.println("Projeto desenvolvido por: ");
+			System.out.println("Dayane Santana - dayanesantana59@gmail.com");
+			System.out.println("github.com/dayanesantana");
+			System.out.println("\n***********************************************");
+
+
+		
+	}
+
+}
